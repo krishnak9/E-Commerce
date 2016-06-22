16a1142224e17a2ead580e4034f6ba457b2e9fa4
Krishna <krishnaklooper@gmail.com>
2016-06-22 11:09:05 +0530

    added mysql as database on development ENV

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,12 @@
 ActiveRecord::Schema.define(version: 20160621162052) do
 
   create_table "products", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.string   "image_url"
-    t.decimal  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.string   "image_url",   limit: 255
+    t.decimal  "price",                     precision: 10
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
   end
 
 end
