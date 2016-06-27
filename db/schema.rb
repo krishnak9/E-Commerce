bbcda5c35d0de17b0ecf6782f58521656634a316
Krishna <krishnaklooper@gmail.com>
2016-06-27 22:26:56 +0530

    finishing changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621162052) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "products", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "description", limit: 65535
-    t.string   "image_url",   limit: 255
-    t.decimal  "price",                     precision: 10
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
+  create_table "payment_method", force: :cascade do |t|
+    t.string "type", limit: 50
+  end
+
+  create_table "sale", force: :cascade do |t|
+    t.integer  "product_id",     limit: 4
+    t.datetime "date_time",                  null: false
+    t.integer  "qty",            limit: 4
+    t.string   "user_email",     limit: 200
+    t.string   "payment_method", limit: 50
   end
 
 end
