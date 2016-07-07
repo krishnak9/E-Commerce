fc104f950384a4959040790cf15d900a8dcca985
Krishna <krishnaklooper@gmail.com>
2016-07-07 20:38:21 +0530

    limited bg data

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,10 +1,13 @@
 class SalesController < ApplicationController
   
   def getdata
-    
+    query = "select * from sale limit 100;"
+    results = ActiveRecord::Base.connection.execute(query)
+    return results
   end
   
   def index
-     
+     sale_data = getdata
+     @sales = sale_data.to_json
   end
 end
